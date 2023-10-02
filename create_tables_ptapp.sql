b8c6e097ff1259dc6a9dcc01e69a685d7905a3b9
msafarty <michaelsafarty@gmail.com>
2023-10-01 21:19:37 -0400

    changes; function easier to work with, sql changes, added JPTAA context

--- create_tables_ptapp.sql
+++ create_tables_ptapp.sql
@@ -9,15 +9,15 @@ DROP TABLE IF EXISTS responses_detail, questionnaire, questions, sub_questions, 
 GO
 
 CREATE TABLE responses_detail
-    ([questionnaire_id] int, [jurisdiction_id] int, [year] int, [question_id] int, [category_id] int, [sub_question_id] varchar(5), [response] varchar(62))
+    ([questionnaire_id] int, [jurisdiction_id] int, [year] int, [question_id] int, [category_id] int, [sub_question_id] varchar(max), [response] varchar(max))
 ;
     
 CREATE TABLE questionnaire
-    ([questionnaire_id] int, [questionnaire_name] varchar(11))
+    ([questionnaire_id] int, [questionnaire_name] varchar(max))
 GO
     
 CREATE TABLE questions
-    ([questionnaire_id] int, [year] int, [question_id] int, [question_num] int, [question_desc] varchar(129))
+    ([questionnaire_id] int, [year] int, [question_id] int, [question_num] int, [question_desc] varchar(max))
 GO
     
 CREATE TABLE responses
@@ -29,10 +29,10 @@ CREATE TABLE responses_categories
 GO
 
 CREATE TABLE sub_questions
-    ([questionnaire_id] int, [year] int, [question_id] int, [category_id] int, [sub_question_id] int, [sub_question] varchar(64))
+    ([questionnaire_id] int, [year] int, [question_id] int, [category_id] int, [sub_question_id] int, [sub_question] varchar(max))
 GO
     
 CREATE TABLE jurisdictions
-    ([jurisdiction_id] int, [jurisdiction_name] varchar(11), [jurisdiction_description] varchar(49), [jurisdiction_country] varchar(3), [jurisdiction_type] varchar(6))
+    ([jurisdiction_id] int, [jurisdiction_name] varchar(max), [jurisdiction_description] varchar(max), [jurisdiction_country] varchar(max), [jurisdiction_type] varchar(max))
 GO
    
